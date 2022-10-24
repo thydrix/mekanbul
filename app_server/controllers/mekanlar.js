ac485fc9ba42225c9e4b7d271f7e2396c551d5be
thydrix <sahinleryagiz@gmail.com>
2022-10-24 23:17:17 +0300

    ödev3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -3,10 +3,76 @@ var router = express.Router();
 
 /* GET home page. */
 const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', { title: 'Anasayfa' });
+    res.render('anasayfa',
+    { 
+        "baslik": 'Anasayfa',
+        "sayfaBaslik": {
+            "siteAd": "MekanBul",
+            "slogan": "Civardaki Mekanları Keşfet!"
+        },
+        "mekanlar":[
+            {
+                "ad": "Starbucks",
+                "puan": "3",
+                "adres": "Centrum Garden AVM",
+                "imkanlar": ["Dünya Kahveleri","Çay","Kek"],
+                "mesafe": "10km"
+            },
+            {
+                "ad": "Barida Cafe",
+                "puan": "4",
+                "adres": "SDÜ Batı Kampüsü",
+                "imkanlar": ["Kahve","Çay","Kek"],
+                "mesafe": "1km"
+            }
+
+        ]
+    }
+    );
 };
 const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
+    res.render('mekanbilgisi',
+    { "baslik": 'Mekan Bilgisi',
+    "mekanBaslik": "Starbucks",
+    "mekanDetay": {
+        "ad": "Starbucks",
+        "adres": "Centrum Garden AVM",
+        "puan":"4",
+        "saatler": [
+            {
+                "gunler": "Pazartesi-Cuma",
+                "acilis": "9:00",
+                "kapanis": "23:00",
+                "kapali": false
+            },
+            {
+                "gunler": "Cumartesi-Pazar",
+                "acilis": "8:00",
+                "kapanis": "22:00",
+                "kapali": false
+            }
+        ],
+        "imkanlar": ["Dünya Kahveleri","Çay","Kek"],
+        "koordinatlar":{
+            "enlem": "37.7",
+            "boylam": "30.5"
+        },
+        "yorumlar":[
+            {
+                "yorumYapan": "Yağız Şahinler",
+                "yorumMetni": "Kötü",
+                "tarih": "20 Ekim 2022",
+                "puan": "1"
+            },
+            {
+                "yorumYapan": "Yağız Şahinler",
+                "yorumMetni": "Ortalama",
+                "tarih": "20 Ekim 2022",
+                "puan": "3"
+            },
+        ]
+    }
+});
 };
 const yorumEkle=function(req, res, next) {
     res.render('yorumekle', { title: 'Yorum Sayfası' });
