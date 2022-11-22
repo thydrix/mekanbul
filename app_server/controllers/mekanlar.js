47d6f13c0bd6ed6c5da698a7fc3e20213c37d888
thydrix <sahinleryagiz@gmail.com>
2022-11-22 20:25:30 +0300

    ödev5

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,85 +1,77 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-const anaSayfa=function(req, res, next) {
-    res.render('anasayfa',
-    { 
-        "baslik": 'Anasayfa',
-        "sayfaBaslik": {
-            "siteAd": "MekanBul",
-            "slogan": "Civardaki Mekanları Keşfet!"
-        },
-        "mekanlar":[
-            {
-                "ad": "Starbucks",
-                "puan": "3",
-                "adres": "Centrum Garden AVM",
-                "imkanlar": ["Dünya Kahveleri","Çay","Kek"],
-                "mesafe": "10km"
-            },
-            {
-                "ad": "Barida Cafe",
-                "puan": "4",
-                "adres": "SDÜ Batı Kampüsü",
-                "imkanlar": ["Kahve","Çay","Kek"],
-                "mesafe": "1km"
-            }
-
-        ]
-    }
-    );
-};
-const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi',
-    { "baslik": 'Mekan Bilgisi',
-    "mekanBaslik": "Starbucks",
-    "mekanDetay": {
-        "ad": "Starbucks",
-        "adres": "Centrum Garden AVM",
-        "puan":"4",
-        "saatler": [
-            {
-                "gunler": "Pazartesi-Cuma",
-                "acilis": "9:00",
-                "kapanis": "23:00",
-                "kapali": false
-            },
-            {
-                "gunler": "Cumartesi-Pazar",
-                "acilis": "8:00",
-                "kapanis": "22:00",
-                "kapali": false
-            }
-        ],
-        "imkanlar": ["Dünya Kahveleri","Çay","Kek"],
-        "koordinatlar":{
-            "enlem": "37.7",
-            "boylam": "30.5"
-        },
-        "yorumlar":[
-            {
-                "yorumYapan": "Yağız Şahinler",
-                "yorumMetni": "Kötü",
-                "tarih": "20 Ekim 2022",
-                "puan": "1"
-            },
-            {
-                "yorumYapan": "Yağız Şahinler",
-                "yorumMetni": "Ortalama",
-                "tarih": "20 Ekim 2022",
-                "puan": "3"
-            },
-        ]
-    }
-});
-};
-const yorumEkle=function(req, res, next) {
-    res.render('yorumekle', { title: 'Yorum Sayfası' });
+const anaSayfa = function (req, res) {
+  res.render("anasayfa", {
+    baslik: "Ana Sayfa",
+    sayfaBaslik: {
+      siteAd: "MekanBul",
+      slogan: "Civardaki Mekanları Keşfet!",
+    },
+    mekanlar: [
+      {
+        ad: "Starbucks",
+        adres: "Centrum Garden",
+        puan: "4",
+        imkanlar: ["Dünya Kahveleri", "Kekler", "Pastalar"],
+        mesafe: "100m",
+      },
+      {
+        ad: "Gloria Jeans",
+        adres: "Sdü Doğu Kampüsü",
+        puan: "3",
+        imkanlar: ["Kahve", "Çay", "Pasta"],
+        mesafe: "5km",
+      },
+    ],
+  });
 };
 
-module.exports={
-    anaSayfa,
-    mekanBilgisi,
-    yorumEkle
-}
+const mekanBilgisi = function (req, res) {
+  res.render("mekanbilgisi", {
+    baslik: "Mekan Bilgisi",
+    mekanBaslik: "Starbucks",
+    mekanDetay: {
+      ad: "Starbucks",
+      adres: "Centrum Garden AVM",
+      puan: "4",
+      imkanlar: ["Dünya Kahveleri", "Kekler", "Pastalar"],
+      koordinatlar: {
+        enlem: "37.7",
+        boylam: "30.5",
+      },
+      saatler: [
+        {
+          gunler: "Pazartesi-Cuma",
+          acilis: "9:00",
+          kapanis: "23:00",
+          kapali: false,
+        },
+        {
+          gunler: "Cumartesi-Pazar",
+          acilis: "10:00",
+          kapanis: "22:00",
+          kapali: false,
+        },
+      ],
+      yorumlar: [
+        {
+          yorumYapan: "Yağız Şahinler",
+          puan: "4",
+          tarih: "20 Ekim 2022",
+          yorumMetni: "Fiyatlar çok fazla artmış ama lezzet çok iyi",
+        },
+      ],
+    },
+  });
+};
+
+const yorumEkle = function (req, res, next) {
+  res.render("yorumekle", { title: "Yorum Ekle" });
+};
+
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+};
